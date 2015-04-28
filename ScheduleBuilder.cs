221d9830d167926e8b1e2a3bb67e48cbdc5da04b
Vince Lee <vslee888+github@gmail.com>
2015-04-28 23:23:18 +0100

    removed foreach loop

--- ScheduleBuilder.cs
+++ ScheduleBuilder.cs
@@ -13,16 +13,33 @@ namespace NICE_SchedulingKata
 
             foreach (var task in parsedTasks)
             {
-                foreach (var dependency in parsedDependencies)
+                var dep = parsedDependencies.SingleOrDefault(x => x.Child.Id == task.Id);
+                if (dep != null)
                 {
-                    if (dependency.Child.Id != task.Id) continue;
-                    var parentIndex = orderedTasks.FindIndex(x => x.Id == dependency.Parent.Id);
-                    orderedTasks.RemoveAt(parentIndex);
-                    var childIndex = orderedTasks.FindIndex(x => x.Id == dependency.Child.Id);
-                    orderedTasks.Insert(childIndex, dependency.Parent);
+                    orderedTasks.RemoveAt(FindIndexOfDependencyParent(orderedTasks, dep));
+                    orderedTasks.Insert(FindIndexOfDependencyChild(orderedTasks, dep), dep.Parent);
                 }
+
+                //foreach (var dependency in parsedDependencies)
+                //{
+                //    if (dependency.Child.Id != task.Id) continue;
+                //    var parentIndex = orderedTasks.FindIndex(x => x.Id == dependency.Parent.Id);
+                //    orderedTasks.RemoveAt(parentIndex);
+                //    var childIndex = orderedTasks.FindIndex(x => x.Id == dependency.Child.Id);
+                //    orderedTasks.Insert(childIndex, dependency.Parent);
+                //}
             }
             return orderedTasks;
+        }
+
+        private static int FindIndexOfDependencyChild(List<SchedulingTask> orderedTasks, Dependency dep)
+        {
+            return orderedTasks.FindIndex(x => x.Id == dep.Child.Id);
+        }
+
+        private static int FindIndexOfDependencyParent(List<SchedulingTask> orderedTasks, Dependency dep)
+        {
+            return orderedTasks.FindIndex(x => x.Id == dep.Parent.Id);
         }
     }
 }
